fad32305b64ab04b6d017b3e9e2f5e260ee2c539
Rich Harris <hello@rich-harris.dev>
2022-05-06 18:08:05 -0500

    more UI stuff

--- src/lib/client/viewer/FileTree/File.svelte
+++ src/lib/client/viewer/FileTree/File.svelte
@@ -16,7 +16,7 @@
 		/* padding: 0 0 0 1.5em; */
 		/* background: 0 0.1em no-repeat; */
 		/* background-size: 1em 1em; */
-		padding: 0 0 0 1.5em;
+		padding: 0 0 0 0.2em;
 		font-size: 1.6rem;
 		font-family: inherit;
 	}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,43 @@
 <script>
 	import '@sveltejs/site-kit/base.css';
+	import { page, navigating } from '$app/stores';
+	import { Icon, Icons, Nav, NavItem, SkipLink } from '@sveltejs/site-kit';
+	import PreloadingIndicator from '$lib/components/PreloadingIndicator.svelte';
 </script>
 
-<header>learn.svelte.dev</header>
+<Icons />
+
+{#if $navigating && $navigating.to}
+	<PreloadingIndicator />
+{/if}
+
+<SkipLink href="#main" />
+
+<Nav {page} logo="/learn-svelte.svg">
+	<svelte:fragment slot="nav-center">
+		<!-- <NavItem href="/tutorial">Tutorial</NavItem>
+		<NavItem href="/docs">Docs</NavItem>
+		<NavItem href="/examples">Examples</NavItem>
+		<NavItem href="/repl">REPL</NavItem>
+		<NavItem href="/blog">Blog</NavItem>
+		<NavItem href="/faq">FAQ</NavItem> -->
+	</svelte:fragment>
+
+	<svelte:fragment slot="nav-right">
+		<NavItem external="https://kit.svelte.dev">SvelteKit</NavItem>
+
+		<NavItem external="/chat" title="Discord Chat">
+			<span class="small">Discord</span>
+			<span class="large"><Icon name="message-square" /></span>
+		</NavItem>
+
+		<NavItem external="https://github.com/sveltejs/svelte" title="GitHub Repo">
+			<span class="small">GitHub</span>
+			<span class="large"><Icon name="github" /></span>
+		</NavItem>
+	</svelte:fragment>
+</Nav>
+
 <main><slot /></main>
 
 <style>
@@ -22,6 +57,8 @@
 
 	main {
 		width: 100%;
-		height: calc(100vh - var(--header-height));
+		height: calc(100vh - var(--nav-h));
+		position: relative;
+		top: var(--nav-h);
 	}
 </style>
