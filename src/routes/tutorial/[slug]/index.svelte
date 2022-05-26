8f69afb425627d8ae9d5a2a3f63b0ea9c08626c7
Rich Harris <hello@rich-harris.dev>
2022-05-26 19:55:10 -0400

    get URL bar working - closes #25

--- src/routes/tutorial/[slug]/index.svelte
+++ src/routes/tutorial/[slug]/index.svelte
@@ -261,11 +261,10 @@
 
 						<input
 							value={path}
-							readonly
 							on:change={(e) => {
-								// TODO enable URL bar to control iframe
 								const url = new URL(e.currentTarget.value, adapter.base);
 								path = url.pathname + url.search + url.hash;
+								iframe.src = adapter.base + path;
 							}}
 						/>
 					</div>
@@ -405,12 +404,13 @@
 		width: 100%;
 		height: 4rem;
 		display: flex;
+		gap: 0.5rem;
 		padding: 0.4rem;
 		background: #f9f9f9;
 	}
 
 	.chrome button {
-		padding: 0.5rem 1rem;
+		padding: 0.5rem;
 	}
 
 	.chrome button img {
@@ -433,11 +433,6 @@
 		box-shadow: inset 1px 1px 2px rgba(0, 0, 0, 0.1);
 		font-family: inherit;
 		font-size: 1.6rem;
-	}
-
-	/* TODO change this once the input is interactive */
-	.chrome input:focus {
-		outline: none;
 	}
 
 	iframe {
