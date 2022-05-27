4bcf87e3030b9e7f29beee922b0db491fe8052cd
Rich Harris <hello@rich-harris.dev>
2022-05-27 17:54:21 -0400

    get rid of redundant tutorial index page

--- src/routes/tutorial/index.svelte
+++ src/routes/tutorial/index.svelte
@@ -1,83 +1,8 @@
 <script context="module">
-	/** @type {import('./__types/index').Load} */
-	export async function load({ stuff }) {
+	export function load() {
 		return {
-			props: {
-				index: stuff.index
-			}
+			status: 307,
+			redirect: '/tutorial/welcome-to-svelte'
 		};
 	}
 </script>
-
-<script>
-	/** @type {import('$lib/types').PartStub[]} */
-	export let index;
-</script>
-
-<main>
-	<h1>Tutorial</h1>
-
-	{#each index as part, i}
-		<h2>Part {i + 1}. {part.title}</h2>
-
-		<div class="grid">
-			{#each part.chapters as chapter}
-				<section>
-					<h3>{chapter.title}</h3>
-
-					<ul>
-						{#each chapter.sections as section}
-							<li><a href="/tutorial/{section.slug}">{section.title}</a></li>
-						{/each}
-					</ul>
-				</section>
-			{/each}
-		</div>
-	{/each}
-</main>
-
-<style>
-	main {
-		padding: 4rem 1rem;
-		max-width: 96rem;
-		margin: 0 auto;
-	}
-
-	h1,
-	h2 {
-		font-weight: 400;
-		margin: 0 0 0.5em 0;
-	}
-
-	h1 {
-		font-size: 4rem;
-	}
-
-	h3 {
-		border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-		margin: 0 0 0.5em 0;
-	}
-
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		grid-gap: 1rem;
-		margin: 0 0 8rem 0;
-	}
-
-	section {
-		background-color: var(--second);
-		color: white;
-		padding: 1rem;
-		border-radius: 4px;
-	}
-
-	section * {
-		color: white;
-	}
-
-	ul {
-		list-style: none;
-		padding: 0;
-	}
-</style>
