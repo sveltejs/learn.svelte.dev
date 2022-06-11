8f9baf8b104ff7c49ce8322c2ad1397b791e614c
Rich Harris <hello@rich-harris.dev>
2022-06-10 23:11:32 -0400

    slightly nicer error page

---
+++ content/tutorial/03-advanced-svelte/common/src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(body) {
+		font-family: -apple-system, BlinkMacSystemFont,
+			'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue',
+			sans-serif;
+		margin: 0;
+	}
+
+	main {
+		width: 100%;
+		height: 100vh;
+		box-sizing: border-box;
+		padding: 8px;
+	}
+</style>
