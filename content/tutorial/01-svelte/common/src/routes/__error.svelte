2ab7007ce7d14d73d079ff7d4a356b2482631c8c
Rich Harris <hello@rich-harris.dev>
2022-06-10 23:13:10 -0400

    slightly nicer error page (#69)

---
+++ content/tutorial/01-svelte/common/src/routes/__error.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.status === 404}
+	<h1>Not found</h1>
+	<p><a href="/">Go to /</a></p>
+{:else}
+	<p>
+		Server-side rendering failed with HTTP status
+		code
+		<a
+			target="_blank"
+			href="https://httpstatusdogs.com/{$page.status}"
+			>{$page.status}</a
+		>
+	</p>
+
+	<pre>{$page.error?.stack || ''}</pre>
+{/if}
+
+<style>
+	h1 {
+		font-weight: 200;
+		font-size: 2rem;
+		margin: 0 0 0.5em 0;
+	}
+
+	pre {
+		font-family: Menlo, Monaco, monospace;
+		font-size: 13px;
+		background: hsl(0, 100%, 95%);
+		padding: 1rem;
+		overflow-x: auto;
+		border-radius: 5px;
+		color: hsl(0, 100%, 30%);
+	}
+</style>
