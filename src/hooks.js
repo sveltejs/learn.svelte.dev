f4c3f18b5217661fdae7667c829b2b188e91c07a
Rich Harris <hello@rich-harris.dev>
2022-06-12 20:19:49 -0400

    try adding cross-origin CORP policy to enable iframe embedding

--- src/hooks.js
+++ src/hooks.js
@@ -4,6 +4,7 @@ export async function handle({ event, resolve }) {
 
 	response.headers.set('cross-origin-opener-policy', 'same-origin');
 	response.headers.set('cross-origin-embedder-policy', 'require-corp');
+	response.headers.set('cross-origin-resource-policy', 'cross-origin');
 
 	return response;
 }
