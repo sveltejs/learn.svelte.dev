b4ed498fff78a70f2ab0246e4788b955a1bac471
Simon Holthausen <simon.holthausen@vercel.com>
2022-11-28 15:42:45 +0100

    disable csrf for tutorial

--- content/tutorial/common/svelte.config.js
+++ content/tutorial/common/svelte.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {}
+	kit: {
+		csrf: false // needed for the form actions tutorial, web container stuff makes this necessary
+	}
 };
 
 export default config;
