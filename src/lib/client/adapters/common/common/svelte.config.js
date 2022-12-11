9daf010bba83dea74dbee0981751c31c4ab8f450
Rich Harris <hello@rich-harris.dev>
2022-12-10 23:05:46 -0500

    oops, didnt mean to add these

--- src/lib/client/adapters/common/common/svelte.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		csrf: false // needed for the form actions tutorial, web container stuff makes this necessary
-	}
-};
-
-export default config;
