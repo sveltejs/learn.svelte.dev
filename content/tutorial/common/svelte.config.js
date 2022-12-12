60636234364b2a5cbd067847b81c770036d4a165
Rich Harris <hello@rich-harris.dev>
2022-12-12 16:08:27 -0500

    explain CSRF stuff

--- content/tutorial/common/svelte.config.js
+++ content/tutorial/common/svelte.config.js
@@ -1,7 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		csrf: false // needed for the form actions tutorial, web container stuff makes this necessary
+		// For the tutorial, we need to disable CSRF protection.
+		// Don't do this in your own apps unless you know what you're doing!
+		// See https://kit.svelte.dev/docs/configuration#csrf for more info.
+		csrf: false
 	}
 };
 
