e228d7f0e1ae64facb674a662ed34b7bc041021d
Rich Harris <hello@rich-harris.dev>
2022-12-13 15:22:26 -0500

    proxy assets - #125

--- content/tutorial/03-advanced-svelte/10-module-context/02-module-exports/app-a/src/lib/App.svelte
+++ content/tutorial/03-advanced-svelte/10-module-context/02-module-exports/app-a/src/lib/App.svelte
@@ -6,7 +6,7 @@
 
 <!-- https://musopen.org/music/9862-the-blue-danube-op-314/ -->
 <AudioPlayer
-	src="https://sveltejs.github.io/assets/music/strauss.mp3"
+	src="https://learn.svelte.dev/assets/media/music/strauss.mp3"
 	title="The Blue Danube Waltz"
 	composer="Johann Strauss"
 	performer="European Archive"
@@ -14,7 +14,7 @@
 
 <!-- https://musopen.org/music/43775-the-planets-op-32/ -->
 <AudioPlayer
-	src="https://sveltejs.github.io/assets/music/holst.mp3"
+	src="https://learn.svelte.dev/assets/media/music/holst.mp3"
 	title="Mars, the Bringer of War"
 	composer="Gustav Holst"
 	performer="USAF Heritage of America Band"
@@ -22,7 +22,7 @@
 
 <!-- https://musopen.org/music/8010-3-gymnopedies/ -->
 <AudioPlayer
-	src="https://sveltejs.github.io/assets/music/satie.mp3"
+	src="https://learn.svelte.dev/assets/media/music/satie.mp3"
 	title="Gymnopédie no. 1"
 	composer="Erik Satie"
 	performer="Prodigal Procrastinator"
@@ -30,7 +30,7 @@
 
 <!-- https://musopen.org/music/2567-symphony-no-5-in-c-minor-op-67/ -->
 <AudioPlayer
-	src="https://sveltejs.github.io/assets/music/beethoven.mp3"
+	src="https://learn.svelte.dev/assets/media/music/beethoven.mp3"
 	title="Symphony no. 5 in Cm, Op. 67 - I. Allegro con brio"
 	composer="Ludwig van Beethoven"
 	performer="European Archive"
@@ -38,7 +38,7 @@
 
 <!-- https://musopen.org/music/43683-requiem-in-d-minor-k-626/ -->
 <AudioPlayer
-	src="https://sveltejs.github.io/assets/music/mozart.mp3"
+	src="https://learn.svelte.dev/assets/media/music/mozart.mp3"
 	title="Requiem in D minor, K. 626 - III. Sequence - Lacrymosa"
 	composer="Wolfgang Amadeus Mozart"
 	performer="Markus Staab"
