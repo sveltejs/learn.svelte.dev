54066e2d8fde398f42c0f0b4f5820764b8608ef5
Rich Harris <hello@rich-harris.dev>
2022-12-13 15:51:51 -0500

    pass through request headers

--- src/routes/assets/media/[...path]/+server.js
+++ src/routes/assets/media/[...path]/+server.js
@@ -1,5 +1,7 @@
-export async function GET({ params, fetch }) {
-	const response = await fetch(`https://sveltejs.github.io/assets/${params.path}`);
+export async function GET({ request, params, fetch }) {
+	const response = await fetch(`https://sveltejs.github.io/assets/${params.path}`, {
+		headers: request.headers
+	});
 
 	const headers = new Headers(response.headers);
 	headers.set('cross-origin-resource-policy', 'cross-origin');
