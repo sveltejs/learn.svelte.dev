6c7bacc8f47b6516abb1dbc95457f65d98202404
Rich Harris <hello@rich-harris.dev>
2022-12-13 15:57:31 -0500

    be more selective

--- src/routes/assets/media/[...path]/+server.js
+++ src/routes/assets/media/[...path]/+server.js
@@ -1,13 +1,23 @@
 export async function GET({ request, params, fetch }) {
+	const request_headers = new Headers();
+
+	if (request.header.has('range')) {
+		request_headers.set('range', request.headers.get('range'));
+	}
+
+	if (request.header.has('if-range')) {
+		request_headers.set('if-range', request.headers.get('if-range'));
+	}
+
 	const response = await fetch(`https://sveltejs.github.io/assets/${params.path}`, {
-		headers: request.headers
+		headers: request_headers
 	});
 
-	const headers = new Headers(response.headers);
-	headers.set('cross-origin-resource-policy', 'cross-origin');
+	const response_headers = new Headers(response.headers);
+	response_headers.set('cross-origin-resource-policy', 'cross-origin');
 
 	return new Response(response.body, {
 		status: response.status,
-		headers
+		headers: response_headers
 	});
 }
