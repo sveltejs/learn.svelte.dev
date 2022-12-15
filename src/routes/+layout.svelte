177c8b1a53f8a9e702644cf84d316963b0b8da30
Daniel Deutsch <daniel.deutsch@dear-innovations.com>
2022-12-15 13:49:15 +0100

    fix relative link to discord

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,7 +32,7 @@
 	<svelte:fragment slot="nav-right">
 		<NavItem external="https://kit.svelte.dev">SvelteKit</NavItem>
 
-		<NavItem external="/chat" title="Discord Chat">
+		<NavItem external="https://svelte.dev/chat" title="Discord Chat">
 			<span class="small">Discord</span>
 			<span class="large"><Icon name="message-square" /></span>
 		</NavItem>
