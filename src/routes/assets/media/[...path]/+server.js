8d0376cec2f0a6117726d4af2b5cb88c115353ab
Rich Harris <hello@rich-harris.dev>
2022-12-20 04:51:06 -0500

    [fix] pass through request headers (#132)

--- src/routes/assets/media/[...path]/+server.js
+++ src/routes/assets/media/[...path]/+server.js
@@ -1,11 +1,23 @@
-export async function GET({ params, fetch }) {
-	const response = await fetch(`https://sveltejs.github.io/assets/${params.path}`);
+export async function GET({ request, params, fetch }) {
+	const request_headers = new Headers();
 
-	const headers = new Headers(response.headers);
-	headers.set('cross-origin-resource-policy', 'cross-origin');
+	if (request.headers.has('range')) {
+		request_headers.set('range', request.headers.get('range'));
+	}
+
+	if (request.headers.has('if-range')) {
+		request_headers.set('if-range', request.headers.get('if-range'));
+	}
+
+	const response = await fetch(`https://sveltejs.github.io/assets/${params.path}`, {
+		headers: request_headers
+	});
+
+	const response_headers = new Headers(response.headers);
+	response_headers.set('cross-origin-resource-policy', 'cross-origin');
 
 	return new Response(response.body, {
 		status: response.status,
-		headers
+		headers: response_headers
 	});
 }
