2ef959b7b8ee6eaa3ed4fe5881b64f30c9b3b6d0
Rich Harris <hello@rich-harris.dev>
2023-01-31 11:48:05 -0500

    use idiomatic middleware.js file (#201)

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ export default function middleware(_request, _event) {
 
 	response.headers.set('cross-origin-opener-policy', 'same-origin');
 	response.headers.set('cross-origin-embedder-policy', 'require-corp');
+	response.headers.set('cross-origin-resource-policy', 'cross-origin');
 	response.headers.set('x-middleware-next', '1');
 
 	return response;
