e4db4c2208b81f6232f71d4302354ad0b87357a6
tomoam <29677552+tomoam@users.noreply.github.com>
2023-02-21 08:58:39 +0900

    fix: cross-origin isolation for preview (#214)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+const crossOriginIsolationForPreview = {
+	name: 'cross-origin-isolation-for-preview',
+	configurePreviewServer: (server) => {
+		server.middlewares.use((_, res, next) => {
+			res.setHeader('cross-origin-opener-policy', 'same-origin');
+			res.setHeader('cross-origin-embedder-policy', 'require-corp');
+			res.setHeader('cross-origin-resource-policy', 'cross-origin');
+			next();
+		});
+	}
+};
+
 /** @type {import('vite').UserConfig} */
 export default {
 	build: {
@@ -9,7 +21,10 @@ export default {
 
 	logLevel: 'info',
 
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		crossOriginIsolationForPreview
+	],
 
 	server: {
 		fs: {
