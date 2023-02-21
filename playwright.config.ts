f565e2b88032ada1e9b10111914372cfabc299d9
Rich Harris <hello@rich-harris.dev>
2023-02-20 21:10:32 -0500

    testing something (#221)
    * testing something
    
    * separate build from test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'pnpm build && pnpm preview',
+		command: 'pnpm preview',
 		port: 4173
 	}
 };
