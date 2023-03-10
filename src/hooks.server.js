457da8a1dd5cb8187790fc56a07a4e249aba288d
Rich Harris <hello@rich-harris.dev>
2023-03-10 11:20:21 -0500

    remove type annotations (#247)
    Co-authored-by: Rich Harris <git@rich-harris.dev>

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,3 @@
-/** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
 	const response = await resolve(event);
 
--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
-/** @type {import('./$types').PageLoad} */
 export function load() {
 	throw redirect(307, '/tutorial/welcome-to-svelte');
 }
