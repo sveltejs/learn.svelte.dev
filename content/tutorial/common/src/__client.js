fd0ac1b2f1b604e4c63d619a5065f9c40c77e4fa
Rich Harris <git@rich-harris.dev>
2023-03-15 12:48:28 -0400

    always open external URLs in a new tab

--- content/tutorial/common/src/__client.js
+++ content/tutorial/common/src/__client.js
@@ -93,6 +93,22 @@ window.addEventListener('focusin', (e) => {
 	}
 });
 
+window.addEventListener('click', (e) => {
+	let node = e.target;
+	while (node) {
+		if (node.nodeName === 'A') {
+			const href = node.href;
+			const url = new URL(href);
+
+			if (url.origin !== location.origin) {
+				e.preventDefault();
+				window.open(url, '_blank');
+			}
+		}
+		node = node.parent;
+	}
+});
+
 function ping() {
 	parent.postMessage(
 		{
