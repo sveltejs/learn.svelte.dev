4bb2ad310e35625fa2f935490f5b873d7f698552
Rich Harris <git@rich-harris.dev>
2023-03-22 11:45:12 -0400

    .

--- content/tutorial/01-svelte/04-logic/03-else-if-blocks/README.md
+++ content/tutorial/01-svelte/04-logic/03-else-if-blocks/README.md
@@ -8,7 +8,7 @@ Multiple conditions can be 'chained' together with `else if`:
 /// file: App.svelte
 {#if x > 10}
 	<p>{x} is greater than 10</p>
-{:+++else if+++ 5 > x}
+{:+++else if+++ x < 5}
 	<p>{x} is less than 5</p>
 {:else}
 	<p>{x} is between 5 and 10</p>
--- content/tutorial/01-svelte/04-logic/03-else-if-blocks/app-a/src/lib/App.svelte
+++ content/tutorial/01-svelte/04-logic/03-else-if-blocks/app-a/src/lib/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	let x = 7;
+	let x = 0;
 </script>
+
+<button on:click={() => x += 1}>+1</button>
 
 {#if x > 10}
 	<p>{x} is greater than 10</p>
 {:else}
-	{#if 5 > x}
+	{#if x < 5}
 		<p>{x} is less than 5</p>
 	{:else}
 		<p>{x} is between 5 and 10</p>
