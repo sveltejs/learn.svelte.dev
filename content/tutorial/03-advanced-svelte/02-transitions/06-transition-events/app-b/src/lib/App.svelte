96b96e69d3da57009ca01470e4b498aa76a6e612
Rich Harris <git@rich-harris.dev>
2023-04-02 16:45:14 -0400

    fix formatting

--- content/tutorial/03-advanced-svelte/02-transitions/06-transition-events/app-b/src/lib/App.svelte
+++ content/tutorial/03-advanced-svelte/02-transitions/06-transition-events/app-b/src/lib/App.svelte
@@ -15,12 +15,10 @@
 {#if visible}
 	<p
 		transition:fly={{ y: 200, duration: 2000 }}
-		on:introstart={() =>
-			(status = 'intro started')}
-		on:outrostart={() =>
-			(status = 'outro started')}
-		on:introend={() => (status = 'intro ended')}
-		on:outroend={() => (status = 'outro ended')}
+		on:introstart={() => status = 'intro started'}
+		on:outrostart={() => status = 'outro started'}
+		on:introend={() => status = 'intro ended'}
+		on:outroend={() => status = 'outro ended'}
 	>
 		Flies in and out
 	</p>
