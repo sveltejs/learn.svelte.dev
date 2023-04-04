c8ca952f1823011d8421aa342351eadd696289f3
tomoam <29677552+tomoam@users.noreply.github.com>
2023-04-04 17:57:33 +0900

    Revert "remove onDestroy exercise"
    This reverts commit 4aa9e419f68299c98a32ce9d9a4a361e4d2d9a2a.

---
+++ content/tutorial/01-svelte/07-lifecycle/02-ondestroy/app-a/src/lib/Timer.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onInterval } from './utils.js';
+
+	export let callback;
+	export let interval = 1000;
+
+	onInterval(callback, interval);
+</script>
+
+<p>This component executes a callback every {interval}ms</p>
+
+<style>
+	p {
+		border: 1px solid blue;
+		padding: 5px;
+	}
+</style>
