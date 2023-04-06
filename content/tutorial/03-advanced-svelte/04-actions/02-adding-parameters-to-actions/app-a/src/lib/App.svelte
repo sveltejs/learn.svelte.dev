37c844c27275c6cdf0acaaebd924647e53c1bbb5
Rich Harris <git@rich-harris.dev>
2023-04-05 22:41:40 -0400

    remove some junk

--- content/tutorial/03-advanced-svelte/04-actions/02-adding-parameters-to-actions/app-a/src/lib/App.svelte
+++ content/tutorial/03-advanced-svelte/04-actions/02-adding-parameters-to-actions/app-a/src/lib/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	import tippy from 'tippy.js';
 	import 'tippy.js/dist/tippy.css';
-	import 'tippy.js/themes/light.css';
+	import 'tippy.js/themes/material.css';
 
 	function tooltip(node, params) {
 		const tooltip = tippy(node, params);
@@ -17,6 +17,6 @@
 	}
 </script>
 
-<button use:tooltip={{ content: 'Hello!', theme: 'light' }}>
+<button use:tooltip={{ content: 'Hello!', theme: 'material' }}>
 	Hover me
 </button>
