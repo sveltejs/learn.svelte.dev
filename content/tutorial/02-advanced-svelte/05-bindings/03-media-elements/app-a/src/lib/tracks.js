5b492e38a073422c65af4254e4621d785f882f3c
Rich Harris <git@rich-harris.dev>
2023-04-06 15:18:01 -0400

    media elements

---
+++ content/tutorial/02-advanced-svelte/05-bindings/03-media-elements/app-a/src/lib/tracks.js
@@ -0,0 +1,25 @@
+export const tracks = [
+	{
+		src: 'https://learn.svelte.dev/assets/media/music/strauss.mp3',
+		title: 'The Blue Danube Waltz',
+		artist: 'Johann Strauss'
+	},
+
+	{
+		src: 'https://learn.svelte.dev/assets/media/music/holst.mp3',
+		title: 'Mars, the Bringer of War',
+		artist: 'Gustav Holst'
+	},
+
+	{
+		src: 'https://learn.svelte.dev/assets/media/music/satie.mp3',
+		title: 'Gymnopédie no. 1',
+		artist: 'Erik Satie'
+	},
+
+	{
+		src: 'https://learn.svelte.dev/assets/media/music/mozart.mp3',
+		title: 'Requiem in D minor, K. 626 - III. Sequence - Lacrymosa',
+		artist: 'Wolfgang Amadeus Mozart'
+	}
+];
