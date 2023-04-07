37e1d708d589ca03907700a44ca8682c2cdc640c
Rich Harris <git@rich-harris.dev>
2023-04-07 18:22:14 -0400

    svelte:head exercise

--- content/tutorial/02-advanced-svelte/09-special-elements/08-svelte-head/app-a/src/lib/App.svelte
+++ content/tutorial/02-advanced-svelte/09-special-elements/08-svelte-head/app-a/src/lib/App.svelte
@@ -1,5 +1,14 @@
-<svelte:head>
-	<!-- elements go here -->
-</svelte:head>
+<script>
+	const themes = ['margaritaville', 'retrowave', 'spaaaaace', 'halloween'];
+	let selected = themes[0];
+</script>
 
-<h1>Hello world!</h1>
+<h1>Welcome to my site!</h1>
+
+<select bind:value={selected}>
+	<option disabled>choose a theme</option>
+
+	{#each themes as theme}
+		<option>{theme}</option>
+	{/each}
+</select>
--- content/tutorial/02-advanced-svelte/09-special-elements/08-svelte-head/app-b/src/lib/App.svelte
+++ content/tutorial/02-advanced-svelte/09-special-elements/08-svelte-head/app-b/src/lib/App.svelte
@@ -1,8 +1,18 @@
+<script>
+	const themes = ['margaritaville', 'retrowave', 'spaaaaace', 'halloween'];
+	let selected = themes[0];
+</script>
+
 <svelte:head>
-	<link
-		rel="stylesheet"
-		href="tutorial/dark-theme.css"
-	/>
+	<link rel="stylesheet" href="/stylesheets/{selected}.css" />
 </svelte:head>
 
-<h1>Hello world!</h1>
+<h1>Welcome to my site!</h1>
+
+<select bind:value={selected}>
+	<option disabled>choose a theme</option>
+
+	{#each themes as theme}
+		<option>{theme}</option>
+	{/each}
+</select>
