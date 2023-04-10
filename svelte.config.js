1d175a0791b0e358b20e2656e8813a854f355b01
Rich Harris <hello@rich-harris.dev>
2023-04-10 17:25:56 -0400

    practice what we preach (#351)
    Co-authored-by: Rich Harris <git@rich-harris.dev>

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
+import * as child_process from 'node:child_process';
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ runtime: 'edge' })
+		adapter: adapter({ runtime: 'edge' }),
+
+		version: {
+			name: child_process.execSync('git rev-parse HEAD').toString().trim()
+		}
 	},
 
 	vitePlugin: {
