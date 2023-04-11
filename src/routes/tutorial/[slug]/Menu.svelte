7e47e08e948be2f8b6aa4c930a8cab320f9f3fc1
Rich Harris <git@rich-harris.dev>
2023-04-11 07:38:05 -0400

    bigger tap targets

--- src/routes/tutorial/[slug]/Menu.svelte
+++ src/routes/tutorial/[slug]/Menu.svelte
@@ -351,6 +351,14 @@
 		box-sizing: border-box;
 	}
 
+	header a {
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 0 1rem;
+	}
+
 	a:focus-visible,
 	.exercises button:focus-visible {
 		outline: none;
