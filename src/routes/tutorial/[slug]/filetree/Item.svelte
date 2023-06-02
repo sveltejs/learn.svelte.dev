136e3d01326ae0eb087715c5d72f3beb9fb403e9
Ayub <sir.kokabi@gmail.com>
2023-06-02 20:39:21 +0330

    Improve style of selected item in filetree

--- src/routes/tutorial/[slug]/filetree/Item.svelte
+++ src/routes/tutorial/[slug]/filetree/Item.svelte
@@ -188,24 +188,7 @@
 	}
 
 	[aria-current='true'] {
-		color: var(--prime);
+		color: hsl(15, 100%, 50%);
 	}
 
-	[aria-current='true']::after {
-		content: '';
-		position: absolute;
-		width: 1rem;
-		height: 1rem;
-		top: 0.3rem;
-		right: calc(-0.6rem - 2px);
-		background-color: var(--sk-back-3);
-		border: 1px solid var(--sk-back-4);
-		transform: translate(0, 0.2rem) rotate(45deg);
-		z-index: 2;
-	}
-
-	[aria-current='true']:has(:focus-visible)::after,
-	:global(.mobile-filetree) [aria-current='true']::after {
-		display: none;
-	}
 </style>
