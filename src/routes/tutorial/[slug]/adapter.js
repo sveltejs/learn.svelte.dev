9b19caf5eb29fc6342fdd0ac48612b8f9ec5f895
Tee Ming <chewteeming01@gmail.com>
2024-03-03 01:53:09 +0800

    fix: prevent ios from loading webcontainers (#584)

--- src/routes/tutorial/[slug]/adapter.js
+++ src/routes/tutorial/[slug]/adapter.js
@@ -21,9 +21,22 @@ export const warnings = writable({});
 /** @type {Promise<import('$lib/types').Adapter>} */
 let ready = new Promise(() => {});
 
+let initial_load = true;
+
 if (browser) {
+	load_webcontainer();
+	initial_load = false;
+}
+
+export function load_webcontainer() {
 	ready = new Promise(async (fulfil, reject) => {
 		try {
+			// TODO: remove this when webcontainers are properly supported on iOS
+			// see https://github.com/stackblitz/webcontainer-core/issues/1120
+			if (initial_load && /iphone/i.test(navigator.userAgent)) {
+				throw new Error('iOS does not support WebContainers');
+			}
+
 			const module = await import('$lib/client/adapters/webcontainer/index.js');
 			const adapter = await module.create(base, error, progress, logs, warnings);
 
